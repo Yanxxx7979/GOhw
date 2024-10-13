dfa9687d9fda3cb21673ed6a8c3c78371078ee30
YanX <144879926+Yanxxx7979@users.noreply.github.com>
2024-10-13 15:35:28 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	// "fmt"
+	// "encoding/json"
+	// "net/http"
+
+	// "github.com/gin-gonic/gin"
+	// "slices"
+	// "strconv"
+)
+
+func main() {
+	
+	// class01()
+	// class02()
+	// class02_api()
+	class02_HW()
+}
+
